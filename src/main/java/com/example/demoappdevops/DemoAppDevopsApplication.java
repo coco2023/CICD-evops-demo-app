8722b731440f983c9b3d4b5d043e0d449f786ad0
keyuhan <hankeyu2021@gmail.com>
2023-06-06 12:56:54 -0500

    add demo

---
+++ src/main/java/com/example/demoappdevops/DemoAppDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoappdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoappdevops/DemoAppDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoappdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
